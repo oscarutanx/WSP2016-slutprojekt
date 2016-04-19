991d901be5f2929c55bf81531e4acd9981d58216
oscarutanx <o.reibner@gmail.com>
2016-04-19 13:22:15 +0200

    testing

--- index.php
+++ index.php
@@ -5,5 +5,24 @@
  * Date: 2016-04-05
  * Time: 12:50
  */
+session_start();
+/**require "./views/details.php";*/
 require "./views/head.php";
-require "./views/index.php";
+
+$page="index";
+if (isset($_GET["page"])&&isset($_GET["page"]))
+{
+    $page = $_GET["page"];
+}
+if(file_exists("views/{$page}.php"))
+{
+    require("./views/{$page}.php");
+}
+else
+{
+    require("views/index.php");
+}
+
+
+
+require ("./views/footer.html");
